55205fbeaa4215d81d533e32f28820cb3968f59a
Michael DeGori <michael.degori@gmail.com>
2021-01-14 10:41:53 -0500

    first

--- App.js
+++ App.js
@@ -3,9 +3,11 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log('App executed');
+
   return (
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
+      <Text>Hello, React Native!</Text>
       <StatusBar style="auto" />
     </View>
   );
