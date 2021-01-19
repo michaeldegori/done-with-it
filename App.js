78a9cf850049636224a06ab0de2c552ae3b98789
Michael DeGori <michael.degori@gmail.com>
2021-01-19 13:07:42 -0500

    reset format defaults

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
