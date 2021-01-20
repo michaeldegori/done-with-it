e5139ac870acdd502785d3ba0a1dd5a0d2ade0ea
Michael DeGori <michael.degori@gmail.com>
2021-01-20 11:12:01 -0500

    multiple styles

--- App.js
+++ App.js
@@ -15,11 +15,13 @@ export default function App() {
   const handlePress = () => console.log('Text Pressed!');
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView style={[styles.container, containerStyle]}>
       <Button title="Click me" onPress={() => console.log('Button pressed!')} />
     </SafeAreaView>
   );
 }
+
+const containerStyle = { backgroundColor: 'orange' };
 
 const styles = StyleSheet.create({
   container: {
