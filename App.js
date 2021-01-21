4dd9b3f5e6474db142f3c48b16de7c7921782ee1
Michael DeGori <michael.degori@gmail.com>
2021-01-21 15:17:41 -0500

    working with flex

--- App.js
+++ App.js
@@ -1,47 +1,10 @@
-// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableNativeFeedback,
-  View,
-  Image,
-  SafeAreaView,
-  Alert,
-  Button,
-  Platform,
-  StatusBar,
-} from 'react-native';
+import { View } from 'react-native';
 import {
   useDimensions,
   useDeviceOrientation,
 } from '@react-native-community/hooks';
 
 export default function App() {
-  const { landscape } = useDeviceOrientation();
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: '100%',
-          height: landscape ? '100%' : '30%',
-        }}
-      ></View>
-    </SafeAreaView>
-  );
+  return <View style={{ backgroundColor: '#fff', flex: 1 }}></View>;
 }
-
-const containerStyle = { backgroundColor: 'orange' };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
-  },
-});
