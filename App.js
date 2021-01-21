4fda588acb6f12450ab4ab2d3a106af679761de6
Michael DeGori <michael.degori@gmail.com>
2021-01-21 16:57:45 -0500

    welcome screen bones done

--- App.js
+++ App.js
@@ -1,42 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 import {
   useDimensions,
   useDeviceOrientation,
 } from '@react-native-community/hooks';
 
 export default function App() {
-  return (
-    <View
-      style={{
-        backgroundColor: '#fff',
-        flex: 1,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'dodgerblue',
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'gold',
-          width: 100,
-          height: 100,
-        }}
-      />
-      <View
-        style={{
-          backgroundColor: 'tomato',
-          width: 100,
-          height: 100,
-        }}
-      />
-    </View>
-  );
+  return <WelcomeScreen />;
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,18 +1,46 @@
 import React from 'react';
-import { ImageBackground, StyleSheet } from 'react-native';
+import { Image, ImageBackground, StyleSheet, View, Text } from 'react-native';
 
 const WelcomeScreen = (props) => {
   return (
     <ImageBackground
       source={require('../assets/background.jpg')}
       style={styles.background}
-    ></ImageBackground>
+    >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require('../assets/logo-red.png')} />
+        <Text>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}></View>
+      <View style={styles.registerButton}></View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   background: {
     flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  loginButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#fc5c65',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: 'absolute',
+    top: 70,
+    alignItems: 'center',
+  },
+  registerButton: {
+    width: '100%',
+    height: 70,
+    backgroundColor: '#4ecdc4',
   },
 });
 
