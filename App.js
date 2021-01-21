b8815a81bba44ebb312ed26d6f42c87bd9ea3350
Michael DeGori <michael.degori@gmail.com>
2021-01-21 15:23:25 -0500

    flexbox children

--- App.js
+++ App.js
@@ -6,5 +6,31 @@ import {
 } from '@react-native-community/hooks';
 
 export default function App() {
-  return <View style={{ backgroundColor: '#fff', flex: 1 }}></View>;
+  return (
+    <View
+      style={{
+        backgroundColor: '#fff',
+        flex: 1,
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: 'dodgerblue',
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'gold',
+          flex: 1,
+        }}
+      />
+      <View
+        style={{
+          backgroundColor: 'tomato',
+          flex: 1,
+        }}
+      />
+    </View>
+  );
 }
