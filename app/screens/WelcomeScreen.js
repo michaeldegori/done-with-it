fdf092bc81ed812c597dd53e8756e3e99f19872f
Michael DeGori <michael.degori@gmail.com>
2021-01-21 16:38:03 -0500

    styles added

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, StyleSheet } from 'react-native';
 
-const WelcomeScreen = () => {
+const WelcomeScreen = (props) => {
   return (
     <ImageBackground
       source={require('../assets/background.jpg')}
+      style={styles.background}
     ></ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+  },
+});
 
 export default WelcomeScreen;
