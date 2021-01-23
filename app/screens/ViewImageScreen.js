7d2c1097abcf913ac3e74c5be014f568bc85dde5
Michael DeGori <michael.degori@gmail.com>
2021-01-22 22:47:48 -0500

    close button

--- app/screens/ViewImageScreen.js
+++ app/screens/ViewImageScreen.js
@@ -15,6 +15,11 @@ const ViewImageScreen = () => {
 };
 
 const styles = StyleSheet.create({
+  closeIcon: {
+    width: 50,
+    height: 50,
+    backgroundColor: '#fc5c65',
+  },
   container: {
     backgroundColor: '#000',
     flex: 1,
