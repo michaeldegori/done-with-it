4ae356ed7865495d22513a733d91288fadea9f99
Michael DeGori <michael.degori@gmail.com>
2021-01-22 22:38:10 -0500

    import screen to app

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import {
   useDimensions,
   useDeviceOrientation,
